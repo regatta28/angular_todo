aabb0910bda6a82035c150e04f1d23b1db7a5777
Rebecca Stevens <rsbjshan@gmail.com>
2017-09-23 14:16:21 +0100

    Module TodoService in service.js

--- js/service.js
+++ js/service.js
@@ -1,10 +1,10 @@
-angular.module('UserService', [])
-    .factory('UserAPIService', function($http) {
- 
-        UserAPIService = {
-            registerUser: function(url, data) {
-                return $http.post(url, data);
+angular.module('TodoService', [])
+    .factory('TodoAPIService', function($http) {
+        TodoAPIService = {
+            getTodos: function(url, data, token) {
+                var header = "Authorization: JWT " + token;
+                return $http.get(url, {params:{"username": data}}, header);
             }
         };
-        return UserAPIService;
+        return TodoAPIService;
     });
