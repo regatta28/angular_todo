be1845f20f4d4b241092bbf43877e0f74b90a92e
Rebecca Stevens <rsbjshan@gmail.com>
2017-09-23 13:55:55 +0100

    bower install command   a0-angular-storage

--- js/controller.js
+++ js/controller.js
@@ -5,13 +5,30 @@ angular.module('RouteControllers', [])
     .controller('RegisterController', function($scope, UserAPIService) {
  
         $scope.registrationUser = {};
+        var URL = "https://morning-castle-91468.herokuapp.com/";
+ 
+        $scope.login = function() {
+            UserAPIService.callAPI(URL + "accounts/api-token-auth/", $scope.data).then(function(results) {
+                $scope.token = results.data.token;
+                console.log($scope.token);
+            }).catch(function(err) {
+                console.log(err.data);
+            });
+        }
  
         $scope.submitForm = function() {
             if ($scope.registrationForm.$valid) {
                 $scope.registrationUser.username = $scope.user.username;
                 $scope.registrationUser.password = $scope.user.password;
-            }
  
-            console.log($scope.registrationUser.username + " " + $scope.registrationUser.password);
+                UserAPIService.callAPI(URL + "accounts/register/", $scope.registrationUser).then(function(results) {
+                    $scope.data = results.data;
+                    alert("You have successfully registered to Angular Todo");
+                    $scope.login();
+                }).catch(function(err) {
+                    alert("Oops! Something went wrong!");
+                    console.log(err);
+                });
+            }
         }
     });
