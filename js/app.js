6eb5be07f787fae965bb1d0404cdd7157c199c6e
Rebecca Stevens <rsbjshan@gmail.com>
2017-09-25 09:23:55 +0100

    Added edit-todo.html

--- js/app.js
+++ js/app.js
@@ -15,4 +15,8 @@ angular.module('TodoApp').config(function($locationProvider, $routeProvider) {
         templateUrl: 'templates/todo.html',
         controller: 'TodoController'
     });
+    .when('/todo/edit/:id', {
+    templateUrl:'templates/edit-todo.html',
+    controller: 'EditTodoController'
+    });
 });
