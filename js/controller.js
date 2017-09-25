f691cf693600c50d581bbdf711e008f8c7330bfe
Rebecca Stevens <rsbjshan@gmail.com>
2017-09-25 09:25:39 +0100

    Added EditTodoController to controller.js

--- js/controller.js
+++ js/controller.js
@@ -71,3 +71,19 @@ angular.module('RouteControllers', [])
             }
         }
     });
+    .controller('EditTodoController', function($scope, $location, $routeParams, TodoAPIService, store) {
+        var id = $routeParams.id;
+        var URL = "https://morning-castle-91468.herokuapp.com/";
+ 
+        $scope.submitForm = function() {
+            if ($scope.todoForm.$valid) {
+                $scope.todo.username = $scope.username;
+ 
+                TodoAPIService.editTodo(URL + "todo/" + id, $scope.todo, store.get('authToken')).then(function(results) {
+                    $location.path("/todo");
+                }).catch(function(err) {
+                    console.log(err);
+                })
+            }
+        }
+    });
