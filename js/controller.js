f2240f6fc38423a444e66e47c0899e4f0c065560
Rebecca Stevens <rsbjshan@gmail.com>
2017-09-27 11:26:47 +0100

    reinstalled angular-route

--- js/controller.js
+++ js/controller.js
@@ -70,7 +70,7 @@ angular.module('RouteControllers', [])
                 });
             }
         }
-    });
+    })
     .controller('EditTodoController', function($scope, $location, $routeParams, TodoAPIService, store) {
         var id = $routeParams.id;
         var URL = "https://morning-castle-91468.herokuapp.com/";
