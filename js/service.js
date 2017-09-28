1b13410414392310d9d93c9f5930d6f2df9a53fb
Rebecca Stevens <rsbjshan@gmail.com>
2017-09-28 13:02:40 +0100

    Scanned for errors and added to gitignore bower components and node modules

--- js/service.js
+++ js/service.js
@@ -12,19 +12,19 @@ angular.module('TodoService', [])
     .factory('TodoAPIService', function($http) {
         TodoAPIService = {
             getTodos: function(url, data, token) {
-                var header = "Authorization: JWT " + token;
+                var header = "Authorization: JWT" + token;
                 return $http.get(url, {params:{"username": data}}, header);
             },
             createTodo: function(url, data, token) {
-                header = "Authorization: JWT " + token;
+                header = "Authorization: JWT" + token;
                 return $http.post(url, data, header);
             },
 	        editTodo: function(url, data, token) {
-	            header = "Authorization: JWT " + token;
+	            header = "Authorization: JWT" + token;
 	            return $http.put(url, data, header);
 	        },
 	        deleteTodo: function(url, token) {
-	            header = "Authorization: JWT " + token;
+	            header = "Authorization: JWT" + token;
 	            return $http.delete(url, token);
 	        }
         };
